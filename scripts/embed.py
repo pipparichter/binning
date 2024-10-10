2413fd308d116f301477c663e7cd7f39346aabfa
Philippa Richter <pips.richter@gmail.com>
2024-10-10 13:56:56 -0700

    Memory stuff

--- scripts/embed.py
+++ scripts/embed.py
@@ -4,38 +4,91 @@ from Bio import SeqIO
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModel
 import argparse
+import numpy as np 
+import zipfile 
+import os 
+import shutil 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def embed(seq, model):
+def embed(seq, model, mean_pool:bool=False):
     encodings = tokenizer([seq], return_tensors='pt')
     with torch.no_grad():
         embedding = model(encodings.input_ids.to(device), output_hidden_states=True).last_hidden_state
-    return embedding.cpu().numpy()
+    embedding = embedding.cpu().numpy()
+
+    if mean_pool:
+        embeddings = np.mean(embeddings, axis=0).ravel()
+        assert len(embeddings) == 1280, 'embed: The mean-pooled embeddings are the wrong shape.'
+    
+    return embeddings 
+
+
+def get_memory_usage(input_path:str, emb_dim:int=1280, half_precision:bool=False, mean_pool:bool=False): # , dtype=torch.float32):
+    '''Estimate the amount of memory which will be taken up by the embeddings of the sequences in the file.
+    Returns the size in bytes.'''
+    
+    n = 0
+    for record in SeqIO.parse(args.input_path, 'fasta'):
+        n += len(str(record.seq)) if (not mean_pool) else 1
+    # print(f'get_memory_usage: Number of embedded items is {n}.')
+    itemsize = 2 if half_precision else 4 # Get the size of the placeholder in bytes. 
+    return itemsize * n * emb_dim
+    
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--model-name', type=str, default='tattabio/gLM2_650M')
+    # https://github.com/TattaBio/gLM2
     parser.add_argument('--input-path', '-i', type=str, default=None)
-    parser.add_argument('--output-path', '-o', type=str, default=None)
+    parser.add_argument('--output-dir', '-d', type=str, default=None)
     parser.add_argument('--direction', default='+', choices=['+', '-'], type=str)
+    # parser.add_argument('--dtype', choices=['float16', 'float32'], type=str)
+    parser.add_argument('--half-precision', action='store_true')
+    parser.add_argument('--mean-pool', action='store_true')
 
     args = parser.parse_args()
 
+    # torch_dtype = torch.float16 if args.dtype == 'float16' else torch.float32
+    torch_dtype = torch.bfloat16 if args.half_precision else torch.float32
+    file_name, _ = os.path.splitext(os.path.basename(args.input_path))
+    dir_name = os.path.dirname(args.input_path)
+    output_dir = args.output_dir if (args.output_dir is not None) else dir_name
+
+
     # model_name = 'tattabio/gLM2_650M'
     tokenizer = AutoTokenizer.from_pretrained(args.model_name, trust_remote_code=True)
-    model = AutoModel.from_pretrained(args.model_name, trust_remote_code=True).cuda()
+    model = AutoModel.from_pretrained(args.model_name, torch_dtype=torch_dtype, trust_remote_code=True).to(device)
 
-    seq_dict = dict()
-    for record in SeqIO.parse(args.input_path, 'fasta'):
-        seq_dict[record.id] = f'<{args.direction}>{str(record.seq).lower()}'
+    mem = get_memory_usage(args.input_path, half_precision=args.half_precision, mean_pool=args.mean_pool)
+    print('Predicted memory usage of embeddings:', np.round(mem / 1e9, 2), 'GB')
 
-    embs = {}
-    for id_, seq in tqdm(seq_dict.items(), desc='Embedding sequences...'):
-        embs[id_] = embed(seq, model)
+    # If we are mean-pooling, memory is less of a concern, and we can keep everything in memory at once. 
+    if args.mean_pool: 
+        embs = dict()
+        for record in tqdm(SeqIO.parse(args.input_path, 'fasta'), desc='Embedding sequences...'):
+            seq = f'<{args.direction}>{str(record.seq).lower()}'
+            embs[record.id] = embed(seq, model, mean_pool=args.mean_pool)
 
-    with open(args.output_path, 'wb') as file:
-        pickle.dump(embs, file)
+        output_path = os.path.join(output_dir, file_name + f'_{args.model_name.split('/')[-1]}.pkl')
+        with open(output_path, 'wb') as f:
+            pickle.dump(embs, f)
+    
+    # If we are not mean-pooling, we will store eveything as a zip archive. 
+    else:
+        output_path = os.path.join(output_dir, file_name + f'_{args.model_name.split('/')[-1]}.zip')
+        print(f'Creating zipped archive at {output_path}')
+        with zipfile.ZipFile(output_path, 'w') as zf:
 
-    print(f'Embeddings written to {args.output_path}')
+            for record in tqdm(SeqIO.parse(args.input_path, 'fasta'), desc='Embedding sequences...'):
+                seq = f'<{args.direction}>{str(record.seq).lower()}'
+                emb = embed(seq, model, mean_pool=args.mean_pool) # Output of this is a numpy array. 
+                
+                tmp_file_path = os.path.join(output_dir, f'{record.id}.txt')
+                np.savetxt(tmp_file_path, emb)
+
+                zf.write(tmp_file_path) # Add the temporary file to the zip archive. 
+                os.remove(tmp_file_path) # Remove the temporary file. 
+
+    print(f'Embeddings written to {output_path}')
